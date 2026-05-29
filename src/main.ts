977c9a1940745bdae5b0a2fedb633369c43cb97a
Parth Dhamija <parthdhamija101998@gmail.com>
2026-05-29 15:19:21 -0700

    Add files via upload

---
+++ src/main.ts
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './dhamija_s_trading_dashboard' // Ensure the filename matches exactly
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
